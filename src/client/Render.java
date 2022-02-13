51fe3c553ea4747b0c11ce6abb869e18628e24d5
CR0YD <98400750+CR0YD@users.noreply.github.com>
2022-02-13 18:43:25 +0100

    Fertigstellung der Renderklasse
    -Bilder können rotiert werden
    -Bilder können über renderMapImage in das Map Panel eingefügt werden
    -Bilder können über deleteMapImage aus dem Map Panel entfernt werden
    -Die Klasse Render hat einen eingebauten refreshTimer, der immer in einem bestimmten Intervall das Bild aktualisiert; dieses Intervall kann mit changeRefreshTimer geändert werden (Überlegung, ob der zusätzliche Thread benötigt wird, da das ImagePane eh beim hinzufügen, bzw. entfernen automatisch aktualisiert wird!!!)
    - Für ein Beispiel im Map Panel jeweils einige Male die linke, mittlere und rechte Maustaste drücken und Änderungen beachten

--- src/client/Render.java
+++ src/client/Render.java
@@ -2,6 +2,7 @@ package client;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.TextArea;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -10,6 +11,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
@@ -26,21 +28,51 @@ import classBase.List;
 @SuppressWarnings("serial")
 public class Render extends JFrame {
 
-	private Graphics2D g;
 	private final int FRAME_WIDTH = 1000, FRAME_HEIGHT = (int) (FRAME_WIDTH * 0.75), FRAME_X = 0, FRAME_Y = 0;
-	int q = 0;
+
+	private int right = 0, down = 0, rotation = 0;	//testing purpose variables
+	
+	private int refreshTime = 0;
 
 	private JPanel mapPanel, infoPanel, shortcutPanel;
 
 	public static void main(String[] args) {
-		new Render();
+		new Render(100);
 	}
 
-	public Render() {
+	public Render(int refreshTime) {
 		super("Country Conflict");
 		setLocation(FRAME_X, FRAME_Y);
+		this.refreshTime = refreshTime;
 		addingComponents();
-
+	}
+	
+	public void changeRefreshTime(int refreshTime) {
+		this.refreshTime = refreshTime;
+		try {
+			ImagePane iP = (ImagePane) mapPanel.getComponent(0);
+			iP.changeRefreshTime(refreshTime);
+		} catch (Exception e) {
+			System.err.println("Object mapPanel does not have a ImagePane object at index 0!");
+		}
+	}
+	
+	public void renderMapImage(String name, int x, int y, int z, int width, int height, int rotation) {
+		try {
+			ImagePane iP = (ImagePane) mapPanel.getComponent(0);
+			iP.addImage(name, x, y, z, width, height, rotation);
+		} catch (Exception e) {
+			System.err.println("Object mapPanel does not have a ImagePane object at index 0!");
+		}
+	}
+	
+	public void deleteMapImage(String name, int x, int y, int z, int width, int height, int rotation) {
+		try {
+			ImagePane iP = (ImagePane) mapPanel.getComponent(0);
+			iP.deleteImage(name, x, y, z, width, height, rotation);
+		} catch (Exception e) {
+			System.err.println("Object mapPanel does not have a ImagePane object at index 0!");
+		}
 	}
 
 	private void addingComponents() {
@@ -60,45 +92,12 @@ public class Render extends JFrame {
 
 			@Override
 			public void windowLostFocus(WindowEvent e) {
-				System.out.println("!");
-			}
-
-		});
-
-		addMouseListener(new MouseAdapter() {
-
-			@Override
-			public void mouseClicked(MouseEvent e) {
 
 			}
 
 		});
 
-		addKeyListener(new KeyListener() {
-
-			@Override
-			public void keyTyped(KeyEvent e) {
-
-			}
-
-			@Override
-			public void keyReleased(KeyEvent e) {
-
-			}
-
-			@Override
-			public void keyPressed(KeyEvent e) {
-
-			}
-		});
-
-		BufferedImage img = null;
-		try {
-			img = ImageIO.read(new File("images/tmp.png"));
-		} catch (Exception e) {
-			System.err.println("!");
-		}
-		ImagePane pane = new ImagePane();
+		ImagePane pane = new ImagePane(refreshTime);
 		pane.addImage("tmp", 0, 0, 0, 710, 500, 0);
 		pane.addImage("tmp2", 100, 10, 1, 400, 350, 0);
 		pane.addImage("tmp3", 150, 70, 2, 400, 350, 0);
@@ -128,12 +127,18 @@ public class Render extends JFrame {
 
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				System.out.println(e.getX() + ", " + e.getY() + "; " + q);
-				pane.deleteImage("tmp3", 150 + (10 * q), 70, 2, 400, 350, 0);
-				q++;
-				pane.addImage("tmp3", 150 + (10 * q), 70, 2, 400, 350, 0);
+				pane.deleteImage("tmp3", 150 + (5 * right), 70 + (5 * down), 2, 400, 350, rotation);
+				if (e.getButton() == 1) {
+					right++;
+				} else if (e.getButton() == 2) {
+					rotation += 10;
+				} else if (e.getButton() == 3) {
+					down++;
+				}
+				pane.addImage("tmp3", 150 + (5 * right), 70 + (5 * down), 2, 400, 350, rotation);
 			}
 		});
+		
 		mapPanel.setBorder(BorderFactory.createTitledBorder("Map"));
 		GroupLayout mapPanelLayout = new GroupLayout(mapPanel);
 		mapPanel.setLayout(mapPanelLayout);
@@ -184,16 +189,20 @@ public class Render extends JFrame {
 		System.err.println("Init exit!");
 		System.exit(0);
 	}
-	
+
 	private final class ImagePane extends JPanel {
 
 		private List<ImageStorage> images = new List<>();
 		private List<ImageParameterList> imageParameter = new List<>();
 		private RenderTimer rT;
+
+		public ImagePane(int refreshTime) {
+			rT = new RenderTimer(refreshTime);
+			rT.start();
+		}
 		
-		public ImagePane() {
-			rT = new RenderTimer(500);
-			rT.run();
+		public void changeRefreshTime(int refreshTime) {
+			rT.changeRefreshTime(refreshTime);
 		}
 
 		public void addImage(String name, int x, int y, int z, int width, int height, int rotation) {
@@ -216,8 +225,10 @@ public class Render extends JFrame {
 			if (!imageIsInList) {
 				images.append(new ImageStorage(img, name));
 				ImageParameterList list = new ImageParameterList(name);
-				list.append(new Parameter(img, x, y, z, width, height, rotation));
+				Parameter param = new Parameter(img, x, y, z, width, height, rotation);
+				list.append(param);
 				imageParameter.append(list);
+				repaint();
 				return;
 			}
 
@@ -232,7 +243,6 @@ public class Render extends JFrame {
 						if (list.getContent().x == x && list.getContent().y == y && list.getContent().z == z
 								&& list.getContent().width == width && list.getContent().height == height
 								&& list.getContent().rotation == rotation) {
-							System.out.println("!**");
 							imageParameterIsInList = true;
 							break;
 						}
@@ -256,6 +266,14 @@ public class Render extends JFrame {
 					images.getContent().decreaseCounter();
 					if (images.getContent().getCounter() < 1) {
 						images.remove();
+						imageParameter.toFirst();
+						while (imageParameter.hasAccess()) {
+							if (imageParameter.getContent().name.equals(name)) {
+								imageParameter.remove();
+								break;
+							}
+							imageParameter.next();
+						}
 					}
 					imageIsInList = true;
 				}
@@ -269,20 +287,17 @@ public class Render extends JFrame {
 			while (imageParameter.hasAccess()) {
 				if (imageParameter.getContent().name.equals(name)) {
 					ImageParameterList list = imageParameter.getContent();
-					System.out.println("**!");
 					list.toFirst();
 					while (list.hasAccess()) {
-						System.out.println("!");
 						if (list.getContent().x == x && list.getContent().y == y && list.getContent().z == z
 								&& list.getContent().width == width && list.getContent().height == height
 								&& list.getContent().rotation == rotation) {
 							list.remove();
-							System.out.println("*!");
-							repaint();
 							return;
 						}
 						list.next();
 					}
+					repaint();
 					return;
 				}
 				imageParameter.next();
@@ -291,9 +306,9 @@ public class Render extends JFrame {
 
 		@Override
 		protected void paintComponent(Graphics g) {
-			boolean inserted;
 			super.paintComponent(g);
-
+			boolean inserted;
+			
 			List<Parameter> sortedParameter = new List<>();
 			imageParameter.toFirst();
 			while (imageParameter.hasAccess()) {
@@ -323,37 +338,51 @@ public class Render extends JFrame {
 			}
 
 			Graphics2D g2d = (Graphics2D) g.create();
+			
+			// for smoother rendering
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			
+			AffineTransform old = null;	//used to store the original rotation value
 			sortedParameter.toFirst();
 			while (sortedParameter.hasAccess()) {
 				Parameter parameter = sortedParameter.getContent();
 				if (parameter.img != null) {
+					old = g2d.getTransform();
+					g2d.rotate(Math.toRadians(parameter.rotation), parameter.x + (parameter.width / 2), parameter.y + (parameter.height / 2));
 					g2d.drawImage(parameter.img, parameter.x, parameter.y, parameter.width, parameter.height, this);
+					g2d.setTransform(old);
 				}
 				sortedParameter.next();
 			}
 			g2d.dispose();
 		}
 
-		private final class RenderTimer implements Runnable {
-			
-			public final int renderTime;
-			
-			public RenderTimer(int renderTime) {
-				this.renderTime = renderTime;
+		private final class RenderTimer extends Thread {
+
+			public int refreshTime;
+
+			public RenderTimer(int refreshTime) {
+				this.refreshTime = refreshTime;
 			}
 
 			@Override
 			public void run() {
-				try {
-					Thread.sleep(renderTime);
-				} catch (Exception e) {
-					System.err.println("Something wrong with the refreshing timer!");
+				while (true) {
+					try {
+						Thread.sleep(refreshTime);
+					} catch (Exception e) {
+						System.err.println("Something wrong with the refreshing timer!");
+					}
+					repaint();
 				}
-				repaint();
 			}
 			
+			public void changeRefreshTime(int refreshTime) {
+				this.refreshTime = refreshTime;
+			}
+
 		}
-		
+
 		private class ImageStorage {
 
 			public final BufferedImage img;
